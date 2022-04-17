26a4a70369b51b0f7317ee9198f7f804fd1476c7
JJJ4032002 <rp4888888@gmail.com>
2022-04-17 15:55:47 +0530

    Add signIn Form

--- src/Components/AuthenticationPageComponents/SignInForm.tsx
+++ src/Components/AuthenticationPageComponents/SignInForm.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Overlay, FormContainer } from "./SignUpFormCss";
+import { UpperContainer } from "./SignUpFormComponents/UpperContainerCss";
+import { Image } from "../AuthenticationPageCss";
+import logo from "../../assets/Icons/logo.svg";
+import close from "../../assets/Icons/close.svg";
 function SignInForm() {
   return (
-    <Overlay>
-      <FormContainer></FormContainer>
+    <Overlay display="flex">
+      <FormContainer>
+        <UpperContainer>
+          <Image id="closeImg" src={close}></Image>
+          <Image id="logoInForm" src={logo}></Image>
+        </UpperContainer>
+      </FormContainer>
     </Overlay>
   );
 }
