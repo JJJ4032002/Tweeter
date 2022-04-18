3fb774ab89240cc6c16fbe074099f3c887cdfa7f
JJJ4032002 <rp4888888@gmail.com>
2022-04-18 14:32:08 +0530

    Add new LabelComponent

---
+++ src/Components/AuthenticationPageComponents/SignUpFormComponents/MiddleContainerComponents/LabelComponent.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Label, SpanText, Input } from "./LabelComponentCss";
+import { InputStateProps } from "../../../../Interfaces and Types/Types";
+function LabelComponent() {
+  let styles = {
+    type: "name",
+    WhichState: "",
+  };
+  return (
+    <Label WhichState={styles.WhichState} htmlFor={type}>
+      <Input
+        value={InputStates[type as keyof InputStateProps]}
+        id={type}
+        onChange={InputChange}
+        name={type}
+        onFocus={FocusAchieved}
+        onBlur={BlurAchieved}
+        type={FinType}
+      />
+      <SpanText WhichState={styles.WhichState}>{SpanName}</SpanText>
+    </Label>
+  );
+}
+
+export default LabelComponent;
---
+++ src/Components/AuthenticationPageComponents/SignUpFormComponents/MiddleContainerComponents/LabelComponentCss.tsx
@@ -0,0 +1,89 @@
+import styled, { css } from "styled-components";
+import { Props } from "../../../../Interfaces and Types/Interfaces";
+const Label = styled.label<Props>`
+  border-width: 1px;
+  border-color: #cfd9de;
+  border-style: solid;
+  position: relative;
+  padding: 0.5em 0.5em;
+  border-radius: 0.2em;
+  display: block;
+  width: 100%;
+  transition: border-width 150 ease, border-color 150 ease;
+  background-color: transparent;
+  ${(props) =>
+    props.WhichState === "AllRedFocussed" &&
+    css`
+      border-width: 2px;
+      border-color: red;
+    `}
+  ${(props) =>
+    props.WhichState === "AllRedBlurred" &&
+    css`
+      border-width: 1px;
+      border-color: red;
+    `}
+      ${(props) =>
+    props.WhichState === "AllBlueFocussed" &&
+    css`
+      border-width: 2px;
+      border-color: #2da7ed;
+    `}
+        ${(props) =>
+    props.WhichState === "WithTextBlurred" &&
+    css`
+      border-width: 1px;
+      border-color: #cfd9de;
+    `}
+`;
+const SpanText = styled.span<Props>`
+  left: 1.5%;
+  top: 32%;
+  font-size: 1rem;
+  color: #536471;
+  transition: top 150ms ease, font-size 150ms ease, color 150ms ease;
+  position: absolute;
+  ${(props) =>
+    props.WhichState === "AllRedFocussed" &&
+    css`
+      top: 5%;
+      font-size: 0.8rem;
+      color: red;
+    `}
+  ${(props) =>
+    props.WhichState === "AllRedBlurred" &&
+    css`
+      top: 32%;
+      font-size: 1rem;
+      color: #536471;
+    `}
+     ${(props) =>
+    props.WhichState === "AllBlueFocussed" &&
+    css`
+      top: 5%;
+      font-size: 0.8rem;
+      color: #2da7ed;
+    `}
+      ${(props) =>
+    props.WhichState === "WithTextBlurred" &&
+    css`
+      top: 5%;
+      font-size: 0.8rem;
+      color: #536471;
+    `}
+`;
+const Input = styled.input`
+  width: 100%;
+  -webkit-appearance: none;
+  border: none;
+  font-size: 1.2rem;
+  margin-top: 0.8em;
+  background-color: transparent;
+  position: relative;
+  z-index: 1;
+
+  &:focus {
+    outline: none;
+  }
+`;
+export { Label, SpanText, Input };
