b46bb61498c7ea60109cd2acc7c69394147b5263
JJJ4032002 <rp4888888@gmail.com>
2022-04-23 11:47:29 +0530

    Add labelBlockcomponent test suite

---
+++ src/tests/LabelBlockComponent.test.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom"; // optional
+import userEvent from "@testing-library/user-event";
+import LabelBlockComponent from "../Components/AuthenticationPageComponents/SignUpFormComponents/MiddleContainerComponents/LabelBlockComponent";
+
+describe("Render label component", () => {
+  it("Renders name Label block correctly", () => {
+    const { container } = render(
+      <LabelBlockComponent type="name">What's your name</LabelBlockComponent>
+    );
+    expect(container).toMatchSnapshot();
+  });
+  it("Renders email Label block correctly", () => {
+    const { container } = render(
+      <LabelBlockComponent type="email">
+        Please enter a valid email
+      </LabelBlockComponent>
+    );
+    expect(container).toMatchSnapshot();
+  });
+  it("Renders password Label block correctly", () => {
+    const { container } = render(
+      <LabelBlockComponent type="password">
+        Please enter a password greater than 8 characters
+      </LabelBlockComponent>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
