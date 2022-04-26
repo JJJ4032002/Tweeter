e5402a3bf99046528a2a669f362cd5a487ff3afd
JJJ4032002 <rp4888888@gmail.com>
2022-04-26 19:51:12 +0530

    Create passwordSpan component

---
+++ src/Components/AuthenticationPageComponents/SignUpFormComponents/MiddleContainerComponents/PasswordSpanComponent.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { PasswordSpan } from "../MiddleContainerCss";
+import { PasswordSpanProps } from "../../../../Interfaces and Types/Interfaces";
+function PasswordSpanComponent({
+  showPassword,
+  handleShowPasswordSpan,
+}: PasswordSpanProps) {
+  return (
+    <>
+      {!showPassword ? (
+        <PasswordSpan onClick={handleShowPasswordSpan}>
+          Show Password
+        </PasswordSpan>
+      ) : (
+        <PasswordSpan onClick={handleShowPasswordSpan}>
+          Hide Password
+        </PasswordSpan>
+      )}
+    </>
+  );
+}
+
+export default PasswordSpanComponent;
