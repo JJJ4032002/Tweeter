5e8d4d1a2d3a4d9d78083ba0677bd525d6cda662
JJJ4032002 <rp4888888@gmail.com>
2022-04-27 18:34:15 +0530

    Add signInform Part in lower container

--- src/Components/AuthenticationPageComponents/SignUpFormComponents/LowerContainer.tsx
+++ src/Components/AuthenticationPageComponents/SignUpFormComponents/LowerContainer.tsx
@@ -1,15 +1,35 @@
 import React, { useContext } from "react";
 
 import { FormPropsContext } from "../../../Contexts/SignUpFormContext";
+import { FormContainersProps } from "../../../Interfaces and Types/Interfaces";
 import { SubmitButton, LowerContainerBlock } from "./LowerContainerCss";
-
-function LowerContainer() {
+import { SignInFormPropsContext } from "../../../Contexts/SignInFormContext";
+function LowerContainer({ signIn }: FormContainersProps) {
   let { FinBtnState, handleSubmitBtnClick } = useContext(FormPropsContext);
+  let { SignInFinBtnState, handleSignInSubmitButton } = useContext(
+    SignInFormPropsContext
+  );
   return (
     <LowerContainerBlock>
-      <SubmitButton onClick={handleSubmitBtnClick} FinBtnState={FinBtnState}>
-        Submit
-      </SubmitButton>
+      {!signIn ? (
+        <>
+          <SubmitButton
+            onClick={handleSubmitBtnClick}
+            FinBtnState={FinBtnState}
+          >
+            Submit
+          </SubmitButton>
+        </>
+      ) : (
+        <>
+          <SubmitButton
+            onClick={handleSignInSubmitButton}
+            FinBtnState={SignInFinBtnState}
+          >
+            Submit
+          </SubmitButton>
+        </>
+      )}
     </LowerContainerBlock>
   );
 }
