d70d0ef2fb6ee6dc3551e4ecb1055e764524b847
JJJ4032002 <rp4888888@gmail.com>
2022-04-27 18:33:18 +0530

    Remove unneeded imports from signInForm

--- src/Components/AuthenticationPageComponents/SignInForm.tsx
+++ src/Components/AuthenticationPageComponents/SignInForm.tsx
@@ -1,32 +1,20 @@
 import React, { useContext } from "react";
 import { Overlay, FormContainer } from "./SignUpFormCss";
-import { UpperContainerBlock } from "./SignUpFormComponents/UpperContainerCss";
-import { Image } from "../AuthenticationPageCss";
-import logo from "../../assets/Icons/logo.svg";
-import close from "../../assets/Icons/close.svg";
-import { MiddleContainerBlock } from "./SignUpFormComponents/MiddleContainerCss";
-import LabelComponent from "./SignUpFormComponents/MiddleContainerComponents/LabelComponent";
 import { AuthenticationPageContext } from "../../Contexts/AuthenticationPageContext";
-import { SignInFormPropsContext } from "../../Contexts/SignInFormContext";
-import PasswordSpanComponent from "./SignUpFormComponents/MiddleContainerComponents/PasswordSpanComponent";
 import { UpperContainer } from "./SignUpFormComponents/UpperContainer";
 import MiddleContainer from "./SignUpFormComponents/MiddleContainer";
+import LowerContainer from "./SignUpFormComponents/LowerContainer";
 function SignInForm() {
-  let { signInFormStyles, handleSignUpBtn } = useContext(
-    AuthenticationPageContext
-  );
-  let { SignInResetForm, signInShowPassword, handleSignInShowPasswordSpan } =
-    useContext(SignInFormPropsContext);
+  let { signInFormStyles } = useContext(AuthenticationPageContext);
+
   let { opacity, display } = signInFormStyles;
-  let signInpasswordType = "password";
-  if (signInShowPassword) {
-    signInpasswordType = "text";
-  }
+
   return (
     <Overlay display={display} opacity={opacity}>
       <FormContainer>
         <UpperContainer signIn={true}></UpperContainer>
         <MiddleContainer signIn={true}></MiddleContainer>
+        <LowerContainer signIn={true}></LowerContainer>
       </FormContainer>
     </Overlay>
   );
