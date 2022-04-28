d3d7f68d21dec2a2e77012e622a1e924fde01e47
JJJ4032002 <rp4888888@gmail.com>
2022-04-28 21:55:59 +0530

    Add react-router to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import "./App.css";
 import { AuthenticationPage } from "./Components/AuthenticationPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Components/HomePage";
 function App() {
   return (
     <div className="App">
-      <AuthenticationPage></AuthenticationPage>
+      <Routes>
+        <Route
+          path={process.env.PUBLIC_URL + "/"}
+          element={<AuthenticationPage />}
+        ></Route>
+        <Route
+          path={process.env.PUBLIC_URL + "/home"}
+          element={<HomePage></HomePage>}
+        ></Route>
+      </Routes>
     </div>
   );
 }
