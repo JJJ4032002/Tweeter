da51bba2d8b06581919dae992f641817f1c542bc
JJJ4032002 <rp4888888@gmail.com>
2022-05-07 11:18:57 +0530

    Add sections to homepage

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import styled from "styled-components";
+import LeftSection from "./HomePageComponents/LeftSection";
+import MiddleSection from "./HomePageComponents/MiddleSection";
+import RightSection from "./HomePageComponents/RightSection";
+const HomeContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1.5fr 2.5fr 2fr;
+  text-align: center;
+`;
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <HomeContainer>
+      <LeftSection></LeftSection>
+      <MiddleSection></MiddleSection>
+      <RightSection></RightSection>
+    </HomeContainer>
+  );
 }
 
 export default HomePage;
---
+++ src/Components/HomePageComponents/MiddleSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MiddleSection() {
+  return <div>MiddleSection</div>;
+}
+
+export default MiddleSection;
