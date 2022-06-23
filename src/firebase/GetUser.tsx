99c08dcf0b667d892ee766eccd29e327baeeee76
JJJ4032002 <rp4888888@gmail.com>
2022-06-23 14:32:16 +0530

    Get user if they sign Up or sign In

---
+++ src/firebase/GetUser.tsx
@@ -0,0 +1,19 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "./InitializeFirebase";
+import { useEffect } from "react";
+import { User } from "firebase/auth";
+function GetUser(setUserHelper: (user: User) => void) {
+  useEffect(() => {
+    const auth = getAuth(app);
+    let unsubscribe = onAuthStateChanged(auth, (user) => {
+      // Check for user status
+      console.log(user);
+      if (user !== null) {
+        setUserHelper(user);
+      }
+    });
+    return unsubscribe();
+  }, []);
+}
+
+export default GetUser;
