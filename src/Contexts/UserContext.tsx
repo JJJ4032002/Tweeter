245b8e17f06a223c6922fcdaa4f01f5ce7855054
JJJ4032002 <rp4888888@gmail.com>
2022-06-23 14:35:14 +0530

    Change the custom User type to firebase User type

--- src/Contexts/UserContext.tsx
+++ src/Contexts/UserContext.tsx
@@ -1,25 +1,20 @@
 import React, { useState } from "react";
-import { User, UserPropsContext } from "../Interfaces and Types/Interfaces";
+import { UserPropsContext } from "../Interfaces and Types/Interfaces";
 import { OverlayContextProviderChildren } from "../Interfaces and Types/Types";
-
+import { User } from "firebase/auth";
+import GetUser from "../firebase/GetUser";
 let UserContext = React.createContext<UserPropsContext>({
-  user: { displayName: "", email: "", photoURL: "", userId: "" },
-  setUserHelper: (user: User) => {},
+  user: null,
 });
 
 function UserContextProvider({ children }: OverlayContextProviderChildren) {
-  let [user, setUser] = useState({
-    displayName: "",
-    email: "",
-    photoURL: "",
-    userId: "",
-  });
+  GetUser(setUserHelper);
+  let [user, setUser] = useState<User | null>(null);
   function setUserHelper(user: User) {
     setUser(user);
   }
   let contextObj = {
     user: user,
-    setUserHelper: setUserHelper,
   };
   return (
     <UserContext.Provider value={contextObj}>{children}</UserContext.Provider>
