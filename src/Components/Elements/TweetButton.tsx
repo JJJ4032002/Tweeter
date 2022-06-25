3303d7a9e95260cac9c4128a97dd00faf7c839bf
JJJ4032002 <rp4888888@gmail.com>
2022-06-25 15:08:51 +0530

    Add TweetButton Component

---
+++ src/Components/Elements/TweetButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Buttons } from "../AuthenticationPageCss";
+function TweetButton() {
+  return (
+    <Buttons className="TweetButton" primary={true}>
+      Tweet
+    </Buttons>
+  );
+}
+
+export default TweetButton;
