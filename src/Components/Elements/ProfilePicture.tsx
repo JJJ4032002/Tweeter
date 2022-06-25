d0539caa926f63112033c6120c88b6bcc9b3012a
JJJ4032002 <rp4888888@gmail.com>
2022-06-25 15:08:32 +0530

    Add ProfilePicture Component

---
+++ src/Components/Elements/ProfilePicture.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ProfileDiv } from "../HomePageCss";
+import profile from "../../assets/Icons/profile.svg";
+function ProfilePicture({
+  InteractOnClick,
+}: {
+  InteractOnClick: null | (() => void);
+}) {
+  return (
+    <>
+      {InteractOnClick !== null ? (
+        <ProfileDiv className="LeftSectionMobileS">
+          <img onClick={InteractOnClick} src={profile} alt="ProfilePicture" />
+        </ProfileDiv>
+      ) : (
+        <ProfileDiv className="LeftSectionMobileS">
+          <img src={profile} alt="ProfilePicture" />
+        </ProfileDiv>
+      )}
+    </>
+  );
+}
+
+export default ProfilePicture;
