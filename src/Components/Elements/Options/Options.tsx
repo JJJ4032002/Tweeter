a9e40c3a20fbe308f0d587f44512ad2913f3c503
JJJ4032002 <rp4888888@gmail.com>
2022-07-09 16:09:45 +0530

    Refactor Options to add clickable options

--- src/Components/Elements/Options/Options.tsx
+++ src/Components/Elements/Options/Options.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { ItemDiv } from "../../HomePageCss";
-function Options({ children }: { children: React.ReactNode }) {
-  return <ItemDiv className="OptionsText">{children}</ItemDiv>;
+import styled from "styled-components/macro";
+const OptionsLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+`;
+
+function Options({
+  children,
+  LinkTo,
+}: {
+  children: React.ReactNode;
+  LinkTo: string;
+}) {
+  return (
+    <>
+      {LinkTo === "disable" ? (
+        <ItemDiv className="OptionsText">{children}</ItemDiv>
+      ) : (
+        <OptionsLink to={`${process.env.PUBLIC_URL}${LinkTo}`}>
+          <ItemDiv className="OptionsText">{children}</ItemDiv>
+        </OptionsLink>
+      )}
+    </>
+  );
 }
 
 export default Options;
