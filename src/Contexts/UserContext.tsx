c7e350408801ca014e7334574a9a1b151e985704
JJJ4032002 <rp4888888@gmail.com>
2022-07-09 18:38:46 +0530

    Add RouteProfile state

--- src/Contexts/UserContext.tsx
+++ src/Contexts/UserContext.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { UserPropsContext } from "../Interfaces and Types/Interfaces";
 import { OverlayContextProviderChildren } from "../Interfaces and Types/Types";
 import { User } from "firebase/auth";
 import GetUser from "../firebase/GetUser";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 let UserContext = React.createContext<UserPropsContext>({
   user: null,
+  RouteProfile: false,
 });
 
 function UserContextProvider({ children }: OverlayContextProviderChildren) {
   let navigate = useNavigate();
+  let location = useLocation();
+  let [RouteProfile, setRouteProfile] = useState(false);
   GetUser(setUserHelper, handleSuccessfulSignIn);
   let [user, setUser] = useState<User | null>(null);
   function setUserHelper(user: User) {
@@ -18,8 +21,16 @@ function UserContextProvider({ children }: OverlayContextProviderChildren) {
   function handleSuccessfulSignIn() {
     navigate(`${process.env.PUBLIC_URL}/home`);
   }
+  useEffect(() => {
+    if (location.pathname === "/Tweeter/home/profile") {
+      setRouteProfile(true);
+    } else if (location.pathname === "/Tweeter/home") {
+      setRouteProfile(false);
+    }
+  }, [location]);
   let contextObj = {
     user: user,
+    RouteProfile: RouteProfile,
   };
   return (
     <UserContext.Provider value={contextObj}>{children}</UserContext.Provider>
