cf88afe3ecbc511ccc0cfea270e18b70573bc8c2
JJJ4032002 <rp4888888@gmail.com>
2022-07-09 16:07:34 +0530

    Refactor LeftSectionLaptopData

--- src/Data.tsx
+++ src/Data.tsx
@@ -25,18 +25,23 @@ const LeftSectionMobileLData = [
   { imgSrc: profile, key: uuidv4() },
   { imgSrc: settings, key: uuidv4() },
 ];
-const LeftSectionMobileMData = [
-  { imgSrc: home, key: uuidv4(), text: "Home" },
-  { imgSrc: search, key: uuidv4(), text: "Search" },
-  { imgSrc: notifications, key: uuidv4(), text: "Notifications" },
-  { imgSrc: messages, key: uuidv4(), text: "Messages" },
-  { imgSrc: profile, key: uuidv4(), text: "Profile" },
-  { imgSrc: settings, key: uuidv4(), text: "Settings" },
+const LeftSectionLaptopMData = [
+  { imgSrc: home, key: uuidv4(), text: "Home", to: "/home" },
+  { imgSrc: search, key: uuidv4(), text: "Search", to: "disable" },
+  {
+    imgSrc: notifications,
+    key: uuidv4(),
+    text: "Notifications",
+    to: "disable",
+  },
+  { imgSrc: messages, key: uuidv4(), text: "Messages", to: "disable" },
+  { imgSrc: profile, key: uuidv4(), text: "Profile", to: "/home/profile" },
+  { imgSrc: settings, key: uuidv4(), text: "Settings", to: "disable" },
 ];
 
 export {
   sideBarOptions,
   RightSectionMobileData,
   LeftSectionMobileLData,
-  LeftSectionMobileMData,
+  LeftSectionLaptopMData,
 };
