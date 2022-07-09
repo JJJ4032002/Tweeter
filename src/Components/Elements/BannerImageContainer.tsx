4a41bdd2c2e9793037f61274644442db944c76c2
JJJ4032002 <rp4888888@gmail.com>
2022-07-09 16:08:40 +0530

    Refactor BannerImageContainer styles

--- src/Components/Elements/BannerImageContainer.tsx
+++ src/Components/Elements/BannerImageContainer.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import styled from "styled-components/macro";
+import { devices } from "../../Media Queries/Queries";
 const BannerContainer = styled.div`
-  padding: 6em 0;
+  height: 130px;
   background-color: lightgrey;
   grid-area: BannerImage;
   grid-column: 1/3;
+
+  @media ${devices.mobileM} {
+    height: 160px;
+  }
+  @media ${devices.mobileL} {
+    height: 180px;
+  }
+  @media ${devices.tablet} {
+    height: 200px;
+  }
+  @media ${devices.laptop} {
+    height: 250px;
+  }
 `;
 function BannerImageContainer() {
   return <BannerContainer></BannerContainer>;
