ab74d92a775a1a98cc35e2727127f8140412dec5
JJJ4032002 <rp4888888@gmail.com>
2022-08-22 21:09:26 +0530

    Remove Redundant code

--- src/Components/AuthenticationPageComponents/LabelComponent.tsx
+++ src/Components/AuthenticationPageComponents/LabelComponent.tsx
@@ -1,70 +1,9 @@
-import React, { useContext } from "react";
-import { Label, SpanText, Input } from "./LabelComponentCss";
-import { InputStateProps } from "../../Interfaces and Types/Types";
+import React from "react";
+
 import { inputTypeInterface } from "../../Interfaces and Types/Interfaces";
-import { FormPropsContext } from "../../Contexts/SignUpFormContext";
-import { SignInFormPropsContext } from "../../Contexts/SignInFormContext";
-function LabelComponent({ type, inputType }: inputTypeInterface) {
-  let { Styles, FocusAchieved, BlurAchieved, InputChange, InputStates } =
-    useContext(FormPropsContext);
-  let {
-    SignInStyles,
-    signInInputStates,
-    SignInFocusAchieved,
-    SignInBlurAchieved,
-    SignInInputChange,
-  } = useContext(SignInFormPropsContext);
-  let styles = {
-    type: "name",
-    WhichState: "",
-  };
-  if (type === "name") {
-    styles = Styles[0];
-  } else if (type === "email") {
-    styles = Styles[1];
-  } else {
-    styles = Styles[2];
-  }
 
-  if (inputType === "signIn") {
-    InputStates = signInInputStates;
-    FocusAchieved = SignInFocusAchieved;
-    BlurAchieved = SignInBlurAchieved;
-    InputChange = SignInInputChange;
-    if (type === "email") {
-      styles = SignInStyles[0];
-    } else {
-      styles = SignInStyles[1];
-    }
-  }
-  let FinType = "text";
-  if (type === "password") {
-    FinType = "password";
-  } else if (type === "text") {
-    type = "password";
-  }
-  let SpanName = "Name";
-
-  if (type === "email") {
-    SpanName = "Email";
-  } else if (type === "password" || type === "text") {
-    SpanName = "Password";
-  }
-
-  return (
-    <Label WhichState={styles.WhichState} htmlFor={type}>
-      <Input
-        value={InputStates[type as keyof InputStateProps]}
-        id={type}
-        onChange={InputChange}
-        name={type}
-        onFocus={FocusAchieved}
-        onBlur={BlurAchieved}
-        type={FinType}
-      />
-      <SpanText WhichState={styles.WhichState}>{SpanName}</SpanText>
-    </Label>
-  );
+function LabelComponent({ children }: inputTypeInterface) {
+  return <>{children}</>;
 }
 
 export default LabelComponent;
--- src/Components/AuthenticationPageComponents/SignUpFormComponents/MiddleContainerComponents/LabelBlockComponent.tsx
+++ src/Components/AuthenticationPageComponents/SignUpFormComponents/MiddleContainerComponents/LabelBlockComponent.tsx
@@ -1,25 +1,9 @@
-import React, { useContext } from "react";
-import { LabelBlock, ErrorText } from "./LabelBlockComponentCss";
+import React from "react";
+import { LabelBlock } from "./LabelBlockComponentCss";
 import { LabelBlockComponentProps } from "../../../../Interfaces and Types/Types";
-import { FormPropsContext } from "../../../../Contexts/SignUpFormContext";
-import LabelComponent from "../../LabelComponent";
-function LabelBlockComponent({ children, type }: LabelBlockComponentProps) {
-  let { NameErrRef, EmailErrRef, PasswordErrRef } =
-    useContext(FormPropsContext);
 
-  let ref = NameErrRef;
-  if (type === "email") {
-    ref = EmailErrRef;
-  } else if (type === "password" || type === "text") {
-    ref = PasswordErrRef;
-  }
-
-  return (
-    <LabelBlock>
-      <LabelComponent type={type} inputType="signUp"></LabelComponent>
-      <ErrorText ref={ref}>{children}</ErrorText>
-    </LabelBlock>
-  );
+function LabelBlockComponent({ children }: LabelBlockComponentProps) {
+  return <LabelBlock>{children}</LabelBlock>;
 }
 
 export default LabelBlockComponent;
