da981a446ef02e7c319b3f1e6ec739d326dc005b
JJJ4032002 <rp4888888@gmail.com>
2022-08-27 15:07:34 +0530

    Add styled-components macro to get readable classNames

--- src/Components/ProfilePageComponents/ProfileInformation.tsx
+++ src/Components/ProfilePageComponents/ProfileInformation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/macro";
 import FollowTally from "../Elements/FollowTally";
 const ProfileContainer = styled.div`
   display: flex;
