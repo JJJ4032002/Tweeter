d993914dff55aa0d3bc72fe1a84da48ad73e6a37
JJJ4032002 <rp4888888@gmail.com>
2022-08-27 15:04:18 +0530

    Add EditProfile component

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,52 @@ import HomePage from "./Components/HomePage";
 import { UserContextProvider } from "./Contexts/UserContext";
 import ProfilePage from "./Components/ProfilePage";
 import MiddleSection from "./Components/HomePageComponents/MiddleSection";
+import { useEffect, useState } from "react";
+import theme from "./theme";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+const GlobalStyles = createGlobalStyle`
+body{
+  background: ${(props) => props.theme.colors.background};
+  color : ${(props) => props.theme.colors.textPrimary};
+}
+`;
 function App() {
+  let [dispEditProfile, setDispEditProfile] = useState(false);
+  const [themeCurrent, setThemeCurrent] = useState("light");
+  const nextTheme = themeCurrent === "light" ? "dark" : "light";
+
+  useEffect(() => {
+    document.body.dataset.theme = themeCurrent;
+  }, [theme]);
+
+  function handleDispEditProfile(state: boolean) {
+    setDispEditProfile(state);
+  }
   return (
     <UserContextProvider>
-      <div className="App">
-        <Routes>
-          <Route
-            path={process.env.PUBLIC_URL + "/"}
-            element={<AuthenticationPage />}
-          ></Route>
-          <Route
-            path={process.env.PUBLIC_URL + "/home"}
-            element={<HomePage></HomePage>}
-          >
-            <Route index element={<MiddleSection />} />
-            <Route path="profile" element={<ProfilePage />}></Route>
-          </Route>
-        </Routes>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <GlobalStyles></GlobalStyles>
+          <Routes>
+            <Route
+              path={process.env.PUBLIC_URL + "/"}
+              element={<AuthenticationPage />}
+            ></Route>
+            <Route
+              path={process.env.PUBLIC_URL + "/home"}
+              element={<HomePage EditProfileState={dispEditProfile}></HomePage>}
+            >
+              <Route index element={<MiddleSection />} />
+              <Route
+                path="profile"
+                element={
+                  <ProfilePage handleDispEditProfile={handleDispEditProfile} />
+                }
+              ></Route>
+            </Route>
+          </Routes>
+        </div>
+      </ThemeProvider>
     </UserContextProvider>
   );
 }
