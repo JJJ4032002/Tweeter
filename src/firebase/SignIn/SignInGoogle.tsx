1e2410a203b34a401a87845f8077cf4dc64648ea
JJJ4032002 <rp4888888@gmail.com>
2022-09-15 23:08:10 +0530

    Add SignInGoogle - In progress

---
+++ src/firebase/SignIn/SignInGoogle.tsx
@@ -0,0 +1,26 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../InitializeFirebase";
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+
+async function SignInWithGoogle() {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
