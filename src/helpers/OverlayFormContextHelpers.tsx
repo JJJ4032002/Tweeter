5a1833d46d68aa929843dd22f8b05987af361817
JJJ4032002 <rp4888888@gmail.com>
2022-10-26 22:05:28 +0530

    Update invalidInputLengthChecker

--- src/helpers/OverlayFormContextHelpers.tsx
+++ src/helpers/OverlayFormContextHelpers.tsx
@@ -1,16 +1,30 @@
-import { InputValsInterface } from "../Interfaces and Types/Interfaces";
+import { InputValsInterfacePartial } from "../Interfaces and Types/Interfaces";
 
 function invalidInputLengthChecker(
-  inputVals: InputValsInterface,
-  event: React.ChangeEvent<HTMLInputElement>
+  inputVals: InputValsInterfacePartial,
+  event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
 ) {
   if (
     (event.target.name === "name" &&
+      inputVals.name &&
       inputVals.name.length >= 30 &&
       event.target.value > inputVals.name) ||
     (event.target.name === "password" &&
+      inputVals.password &&
       inputVals.password.length >= 15 &&
-      event.target.value > inputVals.password)
+      event.target.value > inputVals.password) ||
+    (event.target.name === "bio" &&
+      inputVals.bio &&
+      inputVals.bio.length >= 160 &&
+      event.target.value > inputVals.bio) ||
+    (event.target.name === "location" &&
+      inputVals.location &&
+      inputVals.location.length >= 160 &&
+      event.target.value > inputVals.location) ||
+    (event.target.name === "website" &&
+      inputVals.website &&
+      inputVals.website.length >= 100 &&
+      event.target.value > inputVals.website)
   ) {
     return true;
   } else {
