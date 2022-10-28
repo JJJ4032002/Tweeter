166ee25eb1119a821170200436f75762131c5ffa
JJJ4032002 <rp4888888@gmail.com>
2022-10-28 23:15:55 +0530

    Update profile information

--- src/Components/Profile/Components/ProfileInformation.tsx
+++ src/Components/Profile/Components/ProfileInformation.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components/macro";
+import { UserContext } from "../../../Contexts/UserContext";
 import FollowTally from "../../Elements/FollowTally";
-const ProfileContainer = styled.div`
+import Img from "../../Elements/Img";
+import link from "../../../assets/Icons/link.svg";
+import location from "../../../assets/Icons/location.svg";
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   padding: 0em 1em;
-  gap: 1em;
+  gap: 0.8em;
 `;
 const UserName = styled.p`
   font-weight: 900;
@@ -13,19 +17,51 @@ const UserName = styled.p`
 `;
 
 const UserBio = styled.p`
-  font-size: 0.8rem;
+  font-size: 1rem;
+`;
+
+const SecondaryInformation = styled.div`
+  display: flex;
+  gap: 1em;
+  align-items: center;
+  font-size: 0.9rem;
+`;
+
+const Item = styled.div`
+  display: flex;
+  gap: 0.5em;
+  align-items: center;
 `;
 function ProfileInformation() {
+  let { user } = useContext(UserContext);
   return (
-    <ProfileContainer>
-      <UserName>Rohan Patel</UserName>
-      <UserBio>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        nemo debitis nulla omnis voluptas magni ipsa pariatur dignissimos,
-        temporibus inventore!
-      </UserBio>
+    <Wrapper>
+      {user && user.name ? (
+        <UserName>{user.name}</UserName>
+      ) : (
+        <UserName>Loading...</UserName>
+      )}
+      {user && user.bio ? <UserBio>{user.bio}</UserBio> : ""}
+      <SecondaryInformation>
+        {user && user.location ? (
+          <Item>
+            <Img className="logo" src={location}></Img>
+            <p>{user.location}</p>
+          </Item>
+        ) : (
+          ""
+        )}
+        {user && user.website ? (
+          <Item>
+            <Img className="logo" src={link}></Img>
+            <a href={user.website}>{user.website}</a>
+          </Item>
+        ) : (
+          ""
+        )}
+      </SecondaryInformation>
       <FollowTally></FollowTally>
-    </ProfileContainer>
+    </Wrapper>
   );
 }
 
